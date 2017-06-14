9fc90034e3fcf1fe52734d4a4d443ece8b7c40c4
equal-l2 <eng.equall2@gmail.com>
2017-06-14 13:31:03 +0900

    Add pages

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,11 @@ fn get_current_dir_contents() -> Vec<String> {
     get_dir_contents(std::env::current_dir().unwrap().as_path())
 }
 
+const PRINT_OFFSET:usize = 4;
+
 struct State<'a> {
     index   :usize,
+    page    :usize,
     content :Vec<String>,
     queue   :Vec<(String,Style)>,
     rb      :&'a rustbox::RustBox
@@ -27,6 +30,7 @@ impl<'a> State<'a> {
     fn new(rb_ref:&rustbox::RustBox) -> State {
         State{
             index:0,
+            page:0,
             content:get_current_dir_contents(),
             queue:vec!(),
             rb:rb_ref
@@ -34,11 +38,23 @@ impl<'a> State<'a> {
     }
 
     fn inc_index(&mut self){
-        if self.index < self.content.len()-1 { self.index += 1; };
+        let items = std::cmp::min(self.rb.height()-PRINT_OFFSET,self.content.len());
+        if self.index < items-1 { self.index += 1; };
     }
 
     fn dec_index(&mut self){
         if self.index > 0  { self.index -= 1; };
+    }
+
+    fn next_page(&mut self){
+        self.index = 0;
+        let pages = self.content.len()/(self.rb.height()-PRINT_OFFSET);
+        if pages != 0 && self.page < pages { self.page += 1;} // WIP
+    }
+
+    fn prev_page(&mut self){
+        self.index = 0;
+        if self.page > 0 { self.page -= 1; }
     }
 
     fn open(&mut self){
@@ -64,6 +80,7 @@ impl<'a> State<'a> {
     }
 
     fn print(&mut self){
+        self.rb.clear();
         for (i, entry) in self.queue.iter().enumerate() {
             let (s,sty) = entry.clone();
             self.rb.print(0,i,sty,rustbox::Color::White,rustbox::Color::Black,s.as_str());
@@ -73,14 +90,19 @@ impl<'a> State<'a> {
     }
 
     fn list_current_dir(&mut self) {
-        self.rb.clear();
+        let pages = self.content.len()/(self.rb.height()-PRINT_OFFSET);
         self.queue.push((std::env::current_dir().unwrap().into_os_string().into_string().unwrap(),rustbox::RB_REVERSE));
+        self.queue.push((format!("Item(s): {} Page(s):{}/{}",self.content.len(),self.page+1,pages+1),rustbox::RB_REVERSE));
         self.queue.push((String::from(""),rustbox::RB_NORMAL));
-        for (i, entry) in self.content.iter().enumerate() {
+
+        let min = self.page*(self.rb.height()-PRINT_OFFSET);
+        for i in 0..(self.rb.height()-PRINT_OFFSET) {
             let sty =
                 if self.index == i { rustbox::RB_REVERSE }
                 else               { rustbox::RB_NORMAL }
             ;
+            if i+min >= self.content.len() { break; }
+            let entry = &self.content[i+min];
             let p = Path::new(entry);
             if std::fs::metadata(p).unwrap().is_dir() {
                 self.queue.push(([entry,"/"].concat(),sty));
@@ -89,12 +111,13 @@ impl<'a> State<'a> {
                 self.queue.push((entry.to_owned(),sty));
             }
         }
+
         self.print();
     }
 }
 
 fn main(){
-    let mut rb = rustbox::RustBox::init(Default::default()).unwrap();
+    let rb = rustbox::RustBox::init(Default::default()).unwrap();
     let mut f = State::new(&rb);
 
     loop {
@@ -108,6 +131,8 @@ fn main(){
                     rustbox::Key::Up |
                     rustbox::Key::Char('k') => { f.dec_index(); }
                     rustbox::Key::Enter     => { f.open();      }
+                    rustbox::Key::Right     => { f.next_page(); }
+                    rustbox::Key::Left      => { f.prev_page(); }
                     _ => { }
                 }
             },
